cfbeb2c3d7fc81505aae4d087498aacdcdff891a
ziolko <aaartiii99@gmail.com>
2021-05-13 13:11:06 +0200

    start classes geometricalBloc and hexagonalPrism

---
+++ inc/GeometricalBlock.h
@@ -0,0 +1,10 @@
+//
+// Created by artur on 5/13/21.
+//
+
+#ifndef DRONE_SCENE_GEOMETRICALBLOCK_H
+#define DRONE_SCENE_GEOMETRICALBLOCK_H
+class geometricalBloc{
+
+};
+#endif //DRONE_SCENE_GEOMETRICALBLOCK_H
---
+++ inc/HexagonalPrism.h
@@ -0,0 +1,10 @@
+//
+// Created by artur on 5/13/21.
+//
+
+#ifndef DRONE_SCENE_HEXAGONALPRISM_H
+#define DRONE_SCENE_HEXAGONALPRISM_H
+class hexagonalPrism: public GeometricalBlock{
+
+};
+#endif //DRONE_SCENE_HEXAGONALPRISM_H
